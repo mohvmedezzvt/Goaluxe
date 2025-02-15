d104c223ab69592e8c1c6b0eba80fb585e6d0838
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2025-02-15 17:01:43 +0200

    Add additional security measures

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -2,6 +2,9 @@ import 'dotenv/config.js';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 import connectDB from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
@@ -15,9 +18,21 @@ const port = process.env.PORT || 3001;
 
 connectDB();
 
+app.use(helmet());
+
+// Rate Limiting: Limit repeated requests to public endpoints.
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(mongoSanitize());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/goals', goalRoutes);
