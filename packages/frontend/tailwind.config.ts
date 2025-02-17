2c77a9ce6ff8bda016cdb50cc8804525dbe65524
Eyad Ahmed <Eyad932004@outlook.com>
2025-02-17 10:30:39 +0200

    refactor: simplify Tailwind CSS config by consolidating theme settings and adding custom colors

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 const { heroui } = require("@heroui/react");
 
 export default {
-  darkMode: "class",
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -57,6 +57,19 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
+      },
+      keyframes: {
+        ripple: {
+          "0%, 100%": {
+            transform: "translate(-50%, -50%) scale(1)",
+          },
+          "50%": {
+            transform: "translate(-50%, -50%) scale(0.9)",
+          },
+        },
       },
     },
   },
