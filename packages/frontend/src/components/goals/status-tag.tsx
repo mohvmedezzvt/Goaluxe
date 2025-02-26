6464e4060095c046d8c1ece2dfde262cccce05de
Eyad Ahmed <Eyad932004@outlook.com>
2025-02-26 08:12:10 +0200

    refactor(frontend):rename status tag file

--- packages/frontend/src/components/goals/status-tag.tsx
+++ packages/frontend/src/components/goals/status-tag.tsx
@@ -7,6 +7,7 @@ const StatusTag = ({ status }: { status: string }) => {
       className={cn(
         "px-2 py-1 rounded-full h-fit text-xs whitespace-nowrap",
         status === "active" && "bg-green-100 text-green-700",
+        status === "pending" && "bg-green-100 text-green-700",
         status === "completed" && "bg-blue-100 text-blue-700",
         status === "cancelled" && "bg-gray-100 text-gray-700"
       )}
