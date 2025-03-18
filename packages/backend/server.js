5b1cbc6b2c7a4e1f806010d4cd7b040e3018b3c4
Eyad Ahmed <Eyad932004@outlook.com>
2025-03-18 19:34:35 +0200

    fix(cors): restrict allowed methods to specific HTTP verbs for improved security

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: 'http://localhost:3000',
-    methods: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow credentials (cookies, authorization headers)
     optionsSuccessStatus: 204,
