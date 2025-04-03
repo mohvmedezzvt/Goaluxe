b5fdf892a64c12ca57b1669b92163ccd4c4c72b2
EyadFezex <Eyad932004@outlook.com>
2025-04-03 15:03:25 +0200

    refactor(status-tag): replace span with Chip component for improved styling

--- packages/frontend/src/components/goals/status-tag.tsx
+++ packages/frontend/src/components/goals/status-tag.tsx
@@ -1,20 +1,22 @@
 import { cn } from "@/lib/utils";
+import { Chip } from "@heroui/react";
 import React from "react";
 
 const StatusTag = ({ status }: { status: string }) => {
   return (
-    <span
+    <Chip
+      size="sm"
       className={cn(
         "px-2 py-1 rounded-full h-fit text-xs whitespace-nowrap ",
         status === "active" && "bg-green-100 text-green-700",
-        status === "in-progress" && "bg-yellow-100 text-yellow-700",
-        status === "pending" && "bg-green-100 text-green-700",
+        status === "in-progress" && "bg-green-100 text-green-700",
+        status === "pending" && "bg-yellow-100 text-yellow-700",
         status === "completed" && "bg-blue-100 text-blue-700",
         status === "cancelled" && "bg-gray-100 text-gray-700"
       )}
     >
       {status}
-    </span>
+    </Chip>
   );
 };
 
